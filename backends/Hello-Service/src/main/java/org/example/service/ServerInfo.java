8eb857c2c666b22241fae9eb94194e3fafa8f178
milindaperera <milindape@gmail.com>
2019-03-01 02:50:11 +0530

    Add backend service

---
+++ backends/Hello-Service/src/main/java/org/example/service/ServerInfo.java
@@ -0,0 +1,21 @@
+package org.example.service;
+
+public class ServerInfo {
+	
+	private String name;
+
+	public ServerInfo(String name) {
+		super();
+		this.name = name;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	
+}
